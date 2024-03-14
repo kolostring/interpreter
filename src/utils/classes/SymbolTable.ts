b8753f8d8b15a2e494508b07c68b6b6261cfc586
kolostring <yoerodriguezjunior@gmail.com>
2024-03-14 13:29:36 -0400

    feat(Semantic): get symbol name by AST

--- src/utils/classes/SymbolTable.ts
+++ src/utils/classes/SymbolTable.ts
@@ -1,7 +1,8 @@
+import { TokenKind } from "../constants/tokenKinds";
 import { AbstractSyntaxTree } from "./AST/AbstractSyntaxTree";
+import { VariableSyntaxTree } from "./AST/VariableSyntaxTree";
 
 export type SymbolType = {
-  name: string;
   scope: number;
   ast: AbstractSyntaxTree;
 };
@@ -9,16 +10,35 @@ export type SymbolType = {
 export default class SymbolTable {
   private symbols: SymbolType[] = [];
 
-  public findSymbol(symbolName: string): SymbolType | undefined {
-    return this.symbols.find((symb) => symb.name === symbolName);
+  public getSymbolName(symbol :SymbolType) :string{
+    const fchild = symbol.ast.getChildren()[0];
+    const token = symbol.ast.getToken();
+
+    if(fchild === undefined || fchild === null){
+      throw new Error(`First child of a Symbol's AST cannot be undefined or null.\nToken: <${TokenKind[token.type]}>: ${token.str}, row: ${token.row}, col: ${token.col}`)
+    }
+    
+    if(fchild instanceof VariableSyntaxTree === false){
+      throw new Error(`First child of a Symbol's AST must be a VariableSyntaxTree.\nToken: <${TokenKind[token.type]}>: ${token.str}, row: ${token.row}, col: ${token.col}`)
+    }
+
+    return fchild.getToken().str;
+  }
+
+  public findSymbolByName(symbolName: string): SymbolType | undefined {
+    return this.symbols.find((symb) => this.getSymbolName(symb) === symbolName);
+  }
+
+  public findSymbol(symbol: SymbolType): SymbolType | undefined {
+    return this.symbols.find((symb) => this.getSymbolName(symb) === this.getSymbolName(symbol));
   }
 
   public addSymbol(symbol: SymbolType): void {
-    const foundSymbol = this.findSymbol(symbol.name);
+    const foundSymbol = this.findSymbol(symbol);
     if(foundSymbol === undefined){
       this.symbols.push(symbol);
     }else{
-      throw new Error(`Redefinition of Symbol: "${foundSymbol.name}".\nInitial Definition at row: ${foundSymbol.ast.getToken().row}, col: ${foundSymbol.ast.getToken().col}\nRedefinition at row: ${symbol.ast.getToken().row}, col: ${symbol.ast.getToken().col}`);
+      throw new Error(`Redefinition of Symbol: "${this.getSymbolName(symbol)}".\nInitial Definition at row: ${foundSymbol.ast.getToken().row}, col: ${foundSymbol.ast.getToken().col}\nRedefinition at row: ${symbol.ast.getToken().row}, col: ${symbol.ast.getToken().col}`);
     }
   }
 
